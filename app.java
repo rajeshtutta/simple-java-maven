7f833a16d0e658948663c24c6025eb7ab01b454d
rajeshtutta <rajeshtutta9143@gmail.com>
2026-03-04 12:58:55 +0530

    Add files via upload

---
+++ app.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, Maven Project!");
+
+        // Example using commons-lang3
+        String name = "rajesh";
+        System.out.println(name.toUpperCase());
+    }
+}
