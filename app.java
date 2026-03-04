0c33d6ef752fd5c8a6a3b7ffad2ce114de4bac6b
rajeshtutta <rajeshtutta9143@gmail.com>
2026-03-04 13:13:23 +0530

    Fix typo in greeting message
    Corrected a typo in the greeting message.

--- app.java
+++ app.java
@@ -2,10 +2,11 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, Maven Project!");
+        System.out.println("Hlo, Maven Project!");
 
         // Example using commons-lang3
         String name = "rajesh";
         System.out.println(name.toUpperCase());
     }
+
 }
